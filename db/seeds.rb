9ac54b9544b37c693a640c2139f9ca5a55020598
ahmetterturk <ahm.ert@hotmail.com>
2021-07-26 18:15:52 +1000

    seed database with categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+categories = ["Strength Training", "Endurance Training", "Flexibility and Mobility Training", "Cardiovascular Training"]
+categories.each do |training|
+    Category.create(category_name: training)
+end
