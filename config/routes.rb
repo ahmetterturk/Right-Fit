23b8aaac1b7069e9d7ed1ddab4163e485f546abb
ahmetterturk <ahm.ert@hotmail.com>
2021-07-26 18:43:16 +1000

    generate programs controller, add programs views, add routes for controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root "welcome#index"
+
+  get '/programs', to: 'programs#index', as:'programs'
+  get 'programs/new', to: 'programs#new', as:'new_program'
+  post '/programs', to: 'programs#create'
+  get '/programs/:id', to: 'programs#show', as: 'program'
+  get '/programs/:id/edit', to: 'programs#edit', as: 'edit_program'
+  patch '/programs/:id', to: 'programs#update'
+  delete '/programs/:id', to: 'programs#delete'
+
 end
