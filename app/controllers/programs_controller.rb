c3ef01528a4adfe02d1c6e3d0aee048f92f69c67
ahmetterturk <ahm.ert@hotmail.com>
2021-07-27 17:41:00 +1000

    add delete, edit, update functionality to progrms, delete not working, add price to forms

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,7 +1,5 @@
 class ProgramsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :edit, :update, :delete]
-    # before_action :get_program_params
-
 
     def index
         @programs = Program.all 
@@ -17,14 +15,8 @@ class ProgramsController < ApplicationController
     end
 
     def create
-        # @category = Category.first 
         @categories = Category.all
-
         @program = Program.new(get_program_params)
-        puts "----------------------------"
-        puts @program.title
-        puts "----------------------------"
-        # @program.category_id = @category.id
         @program.coach = current_user
         if @program.save
             redirect_to program_path(@program.id)
@@ -34,32 +26,29 @@ class ProgramsController < ApplicationController
         end
     end
 
-    # def edit
-    #     @categories = Category.all
-    #     @program = Program.find(params[:id])
-    # end
+    def edit
+        @categories = Category.all
+        @program = Program.find(params[:id])
+    end
 
-    # def update
-    #     @program = Program.find(params[:id])
-    #     if @program.update(@program_params)
-    #         redirect_to program_path(@program.id)
-    #     else
-    #         render 'edit'
-    #     end
-    # end
+    def update
+        @program = Program.find(params[:id])
+        if @program.update(get_program_params)
+            redirect_to program_path(@program.id)
+        else
+            render 'edit'
+        end
+    end
 
-    # def delete
-    #     @program = Program.find(params[:id])
-    #     @program.destroy
-    #     redirect_to programs_path
-    # end
-
+    def delete
+        @program = Program.find(params[:id])
+        @program.destroy
+        redirect_to programs_path
+    end
 
     private
     def get_program_params
-        params.require(:program).permit(:title, :description, :duration, :content, :category_id, :coach_id, :image)
-        # @program_params = params.permit(:title, :description, :duration, :content, :category_id, :coach_id)
+        params.require(:program).permit(:title, :description, :duration, :content, :price, :category_id, :coach_id, :image)
     end
 
-    
 end
