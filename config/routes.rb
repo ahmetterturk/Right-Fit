f08e4c5c3bded46750ad9b3101e73f99959e8244
ahmetterturk <ahm.ert@hotmail.com>
2021-07-28 11:05:19 +1000

    create users and programs controller, crud for profile controller, display user info on users page

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,22 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   get '/programs', to: 'programs#index', as:'programs'
-  get 'programs/new', to: 'programs#new', as:'new_program'
+  get '/programs/new', to: 'programs#new', as:'new_program'
   post '/programs', to: 'programs#create'
   get '/programs/:id', to: 'programs#show', as: 'program'
   get '/programs/:id/edit', to: 'programs#edit', as: 'edit_program'
   patch '/programs/:id', to: 'programs#update'
   delete '/programs/:id', to: 'programs#delete'
+
+
+  get '/users/:id', to: 'users#page', as: 'user_page'
+
+
+  get '/profiles', to: 'programs#index', as: 'profiles'
+  get '/users/profile', to: 'programs#show', as: 'profile'
+  get '/users/profile/new', to: 'profiles#new', as: 'new_profile'
+  post '/profiles', to: 'profiles#create'
+  get '/users/profile/edit', to: 'profiles#edit', as: 'edit_profile'
+  patch '/users/profile', to: 'profiles#update'
 
 end
