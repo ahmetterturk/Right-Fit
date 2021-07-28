20d10b69b5f8acffef3005eda204ed707ac5aaef
ahmetterturk <ahm.ert@hotmail.com>
2021-07-28 11:15:36 +1000

    display coached and attended programs in user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
     def page
-        
+        @coached_programs = current_user.programs_to_coach.all
+        @attended_programs = current_user.programs_to_attend.all
     end
+
+
+
 end
