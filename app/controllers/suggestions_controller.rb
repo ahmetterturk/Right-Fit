5a50e1e5406f22be815a718981ac8e33eb6e93c0
ahmetterturk <ahm.ert@hotmail.com>
2021-07-29 19:14:51 +1000

    build logic in suggestions controller that makes a suggestion to user based on their calculated BMI

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -2,27 +2,30 @@ class SuggestionsController < ApplicationController
     before_action :authenticate_user!, only: [:index]
 
     def index
+        @programs = []
+
         if current_user.profile 
-            if calculated_bmi > 40 
-                @suggested_category = Category.where(category_name: ['Cardiovascular Training'])
+            if calculated_bmi > 31
+                suggested_categories = Category.where(category_name: ['Cardiovascular Training']).to_a
             elsif calculated_bmi > 20 && calculated_bmi < 31
-                @suggested_category = Category.where(category_name: ['Cardiovascular Training', 'Strength Training'])
+                suggested_categories = Category.where(category_name: ['Cardiovascular Training', 'Strength Training']).to_a
             elsif calculated_bmi > 15 && calculated_bmi < 21
-                @suggested_category = Category.where(category_name: ['Endurance Training', 'Flexibility and Mobility Training'])
-            elsif calculated_bmi > 9 && calculated_bmi < 16
-                @suggested_category = Category.where(category_name: ['Flexibility and Mobility Training'])
+                suggested_categories = Category.where(category_name: ['Endurance Training', 'Flexibility and Mobility Training']).to_a
+            elsif calculated_bmi < 16
+                suggested_categories = Category.where(category_name: ['Flexibility and Mobility Training']).to_a
             end
+
+            suggested_categories.each do |category|
+                category.programs.each do |program| 
+                    @programs << program 
+                end 
+            end
+    
+            return @programs
         else
             redirect_to new_profile_path
         end
-
-        puts '======================'
-        # puts @suggested_category.first.programs.each {|program| puts program.title}
-
-        puts '======================'
     end
-
-
 
     private 
     def calculated_bmi
@@ -30,5 +33,11 @@ class SuggestionsController < ApplicationController
         height = user_profile.height 
         weight = user_profile.weight 
         bmi = (weight / ((height / 100) ** 2))
+    end
+
+    def get_programs(category)
+        category.each do |category|
+            category.programs
+        end
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
+    # include BMI 
+
+
     def page
+        @bmi = calculated_bmi
         @coached_programs = current_user.programs_to_coach.all
         @attended_programs = current_user.programs_to_attend.all
     end
 
 
-
+    private 
+    def calculated_bmi
+        user_profile = current_user.profile
+        if !user_profile.nil?
+            height = user_profile.height 
+            weight = user_profile.weight 
+            bmi = (weight / ((height / 100) ** 2))
+        end
+    end
 end
