fa4bb80f92576fd0f71a25448f75d9e66ade6181
ahmetterturk <ahm.ert@hotmail.com>
2021-07-31 22:41:05 +1000

    delete method for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,8 @@
 class ReviewsController < ApplicationController
     before_action :authenticate_user!
-    
+
     def create
         @program = Program.find(params[:program_id])
-        # @review = @program.reviews_to_receive.create(review_params)
         @review = Review.new(review_params)
         @review.user = current_user
         @review.program = @program
@@ -11,9 +10,24 @@ class ReviewsController < ApplicationController
         redirect_to program_path(@program)
     end
 
+    # def show 
+    # end
+
+    # def update 
+    #     program = Program.find(params[:program_id])
+    #     @review = Review.find_by(user: current_user, program: program )
+    #     @review.update(review_params)
+    # end
+
+    def destroy
+        @program = Program.find(params[:program_id])
+        @review = @program.reviews_to_receive.find(params[:id])
+        @review.destroy
+        redirect_to program_path(@program)
+    end
+
     private
     def review_params
         params.require(:review).permit(:content, :title, :rating)
     end
-
 end
