a9e39648efe9d556fe73f614ab7ef8cd96d5dc44
ahmetterturk <ahm.ert@hotmail.com>
2021-08-04 15:34:12 +1000

    edit stripe.js

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,17 @@
 # Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)    
 
 
-Rails.configuration.stripe = {
-    publishable_key: Rails.application.credentials.dig(:stripe, :public_key),
-    secret_key: Rails.application.credentials.dig(:stripe, :secret_key)
-}
+# Rails.configuration.stripe = {
+#     publishable_key: Rails.application.credentials.dig(:stripe, :public_key),
+#     secret_key: Rails.application.credentials.dig(:stripe, :secret_key)
+# }
 
-Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+# Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+
+
+Rails.configuration.stripe = {
+    :publishable_key => ENV['STRIPE_PUBLIC_KEY'],
+    :secret_key => ENV['STRIPE_SECRET_KEY']
+
+}
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
