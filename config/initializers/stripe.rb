b8ec9097c308b93305b020862bba844da4213431
ahmetterturk <ahm.ert@hotmail.com>
2021-08-06 17:08:42 +1000

    remove commented out code in stripe initializer

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,14 +1,3 @@
-# Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)    
-
-
-# Rails.configuration.stripe = {
-#     publishable_key: Rails.application.credentials.dig(:stripe, :public_key),
-#     secret_key: Rails.application.credentials.dig(:stripe, :secret_key)
-# }
-
-# Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
-
-
 Rails.configuration.stripe = {
     :public_key => ENV['STRIPE_PUBLIC_KEY'],
     :secret_key => ENV['STRIPE_SECRET_KEY']
